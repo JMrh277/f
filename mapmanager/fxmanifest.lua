98a5623e8e60820f07cbf1ff3ccfa85ad5aed5f6
JMrh277 <riteishdr@gmail.com>
2024-09-18 05:34:47 -0700

    Add files via upload

---
+++ mapmanager/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version "cerulean"
+games { "gta5" }
+--[[
+
+					███████╗██╗░░░░░██╗██╗░░██╗
+					██╔════╝██║░░░░░██║╚██╗██╔╝
+					█████╗░░██║░░░░░██║░╚███╔╝░
+					██╔══╝░░██║░░░░░██║░██╔██╗░
+					██║░░░░░███████╗██║██╔╝╚██╗
+					╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+
+    Made By : MoSattout#2632
+    ♥ شكرا لك على الثقة ♥
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+client_scripts { "mapmanager_shared.lua", "mapmanager_client.lua" }
+server_scripts { "mapmanager_shared.lua", "mapmanager_server.lua" }
+server_export "getCurrentGameType"
+server_export "getCurrentMap"
+server_export "changeGameType"
+server_export "changeMap"
+server_export "doesMapSupportGameType"
+server_export "getMaps"
+server_export "roundEnded"
+--[[
+
+					███████╗██╗░░░░░██╗██╗░░██╗
+					██╔════╝██║░░░░░██║╚██╗██╔╝
+					█████╗░░██║░░░░░██║░╚███╔╝░
+					██╔══╝░░██║░░░░░██║░██╔██╗░
+					██║░░░░░███████╗██║██╔╝╚██╗
+					╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+
+    Made By : MoSattout#2632
+    ♥ شكرا لك على الثقة ♥
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+
+
+ client_script "RAM.lua"
---
+++ spawnmanager/fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version "cerulean"
+games { "gta5" }
+--[[
+
+					███████╗██╗░░░░░██╗██╗░░██╗
+					██╔════╝██║░░░░░██║╚██╗██╔╝
+					█████╗░░██║░░░░░██║░╚███╔╝░
+					██╔══╝░░██║░░░░░██║░██╔██╗░
+					██║░░░░░███████╗██║██╔╝╚██╗
+					╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+
+    Made By : MoSattout#2632
+    ♥ شكرا لك على الثقة ♥
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+client_script 'spawnmanager.lua'
+export 'getRandomSpawnPoint'
+export 'spawnPlayer'
+export 'addSpawnPoint'
+export 'removeSpawnPoint'
+export 'loadSpawns'
+export 'setAutoSpawn'
+export 'setAutoSpawnCallback'
+export 'forceRespawn'
+--[[
+
+					███████╗██╗░░░░░██╗██╗░░██╗
+					██╔════╝██║░░░░░██║╚██╗██╔╝
+					█████╗░░██║░░░░░██║░╚███╔╝░
+					██╔══╝░░██║░░░░░██║░██╔██╗░
+					██║░░░░░███████╗██║██╔╝╚██╗
+					╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+
+    Made By : MoSattout#2632
+    ♥ شكرا لك على الثقة ♥
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+
+
+ client_script "RAM.lua"
